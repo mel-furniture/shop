d69a597612d68d7b39fde7732a8f867141cb5465
mel-furniture <office@mel.furniture>
2025-11-25 12:17:33 -0600

    Update create-payment-intent.js

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,18 +1,17 @@
 // functions/create-payment-intent.js
-
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15", // or your Stripe API version
-});
+// Use the environment variable
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function onRequestPost({ request }) {
   try {
     const { amount } = await request.json();
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount,           // amount in cents
+      amount,
       currency: "usd",
+      automatic_payment_methods: { enabled: true },
     });
 
     return new Response(JSON.stringify({ clientSecret: paymentIntent.client_secret }), {
