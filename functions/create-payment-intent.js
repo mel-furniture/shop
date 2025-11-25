5f2773d3ddf8553cf17a97912097948da444d1cf
mel-furniture <office@mel.furniture>
2025-11-25 12:05:25 -0600

    Update create-payment-intent.js

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,15 +1,27 @@
-import Stripe from 'stripe';
-const stripe = new Stripe('YOUR_SECRET_KEY');
+// functions/create-payment-intent.js
+
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15", // or your Stripe API version
+});
 
 export async function onRequestPost({ request }) {
-  const { amount } = await request.json();
+  try {
+    const { amount } = await request.json();
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: 'usd'
-  });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,           // amount in cents
+      currency: "usd",
+    });
 
-  return new Response(JSON.stringify({ clientSecret: paymentIntent.client_secret }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
+    return new Response(JSON.stringify({ clientSecret: paymentIntent.client_secret }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
