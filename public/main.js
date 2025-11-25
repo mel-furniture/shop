b2c02ff6381e5dd830e23a2f5a7623a6c0bf962f
mel-furniture <office@mel.furniture>
2025-11-25 14:54:12 -0600

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 // main.js
 import { loadStripe } from "https://js.stripe.com/v3/";
 
-const stripe = Stripe("pk_live_51SVcSEAxCX5hk2YKsiojWL8rpBAiVAZpwMqnUSfQdJFlwtKEOo2JnblmbNvwAr1fUlyLAyXgNxvbBw6yO0pZTzuV00gYr1w8UI"); // replace with your Stripe publishable key
+const stripe = Stripe("pk_live_51SVcSEAxCX5hk2YKsiojWL8rpBAiVAZpwMqnUSfQdJFlwtKEOo2JnblmbNvwAr1fUlyLAyXgNxvbBw6yO0pZTzuV00gYr1w8UI"); // Stripe publishable key
 const paymentForm = document.getElementById("payment-form");
 const cardElementDiv = document.getElementById("card-element");
 const paymentMessage = document.getElementById("payment-message");
